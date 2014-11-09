35e1c331ec0bbacb7c57f8d920f5a1cc12f9b369
Daniel Williams <dwilliams@port8080.net>
2014-11-09 00:05:20 -0700

    file.

---
+++ mms-card-updater.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#
+# mms-card-updater.py
+# A desktop based card updater client to allow quick adding or changing of member's RFID badges.
+
+### IMPORTS ###
+import time
+import serial
+import wx
+
+### GLOBALS ###
+serialPort = ""
+
+### CLASSES ###
+
+### FUNCTIONS ###
+
+### MAIN ###
+def main():
+	# Stuff.
+	pass
+
+if __name__ == '__main__':
+	main()
