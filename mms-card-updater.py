651886fc66b2e02b463bf62f2fbd6150de0850cc
Daniel P Williams <dwilliams@port8080.net>
2015-01-06 16:05:37 -0700

    Basic command line reading of the cards.

--- mms-card-updater.py
+++ mms-card-updater.py
@@ -3,26 +3,75 @@
 # mms-card-updater.py
 # A desktop based card updater client to allow quick adding or changing of member's RFID badges.
 
+# The initial test version will run in the command line and will just output the card reads to the console.
+
 ### IMPORTS ###
 import time
+import threading
 import serial
-import wx
+#import wx
 
 ### GLOBALS ###
-serialPort = ""
+serialPort = "/dev/tty.usbserial-A5027KD1" # /dev/tty* for *nix, COM? for Windows
+baudRate = 9600
+
+connected = False
+exitFlag = False
+
+bufferArray = []
 
 ### CLASSES ###
 
 ### FUNCTIONS ###
+def handle_data( data):
+    # Add the byte to an array and check for a complete packet
+    global bufferArray
+    if( len( data) > 0):
+        bufferArray.append( data)
+        check_packet()
+
+def check_packet():
+    # Check the global array for a packet
+    global bufferArray
+    while( len( bufferArray) > 15):
+        print( "Buffer Length: %d" % ( len( bufferArray), ))
+        # search for the 0x02 char, which is the start of the card
+        try:
+            startIndex = bufferArray.index( '\x02')
+            endIndex = bufferArray.index( '\x03')
+            #print( "       Start Index: %d" % ( startIndex, ))
+            #print( "       End Index: %d" % ( endIndex, ))
+            #print( "       length: %d" % ( endIndex - startIndex, ))
+            print( "       value: %s" % ( ''.join( bufferArray[ startIndex + 1: startIndex + 11]), ))
+            # Remove the card read from the buffer
+            bufferArray = bufferArray[ endIndex + 1: ]
+        except:
+            pass
+
+def read_from_port( ser):
+    global connected
+    global exitFlag
+    while not connected:
+        connected = True
+        
+        while not exitFlag:
+            reading = ser.read() # read a byte
+            handle_data( reading)
 
 ### MAIN ###
 def main():
+    global exitFlag
     # Stuff.
-    app = wx.App( False)  # Create a new app, don't redirect stdout/stderr to a window.
-    frame = wx.Frame( None, wx.ID_ANY, "Hello World")  # A Frame is a top-level window.
-    frame.Show( True)  # Show the frame.
-    app.MainLoop()
-
+    ser = serial.Serial( serialPort, baudRate, timeout = 0)
+    thread = threading.Thread( target = read_from_port, args = ( ser, ))
+    thread.start()
+    
+    while not exitFlag:
+        try:
+            time.sleep( 1)
+        except:
+            # Usually catching the keyboard interrupt
+            exitFlag = True
 
 if __name__ == '__main__':
 	main()
